c80c00552a63a5466348744f015962ccd6cca5a2
Ara-ban <dolipranoob@gmail.com>
2023-02-04 02:50:30 +0100

    thought process for first problem

---
+++ level1/solution.py
@@ -0,0 +1,10 @@
+def solution(s):
+    # Your code here
+    #thought process: 2:29 AM 2/4
+    #first idea is to build a while where there is a counter that starts 
+    #at one and can go up to the length of the list divided by 2(if list 
+    #is even else divided by 3 if dividable by 3 etc..) and take everytime
+    #the first i characters of the string and see if they repeat throughout
+    #the string, else increment i where i is the counter.
+    #now is this a general optimal approach? i will leave the answer for 
+    #tomorrow
